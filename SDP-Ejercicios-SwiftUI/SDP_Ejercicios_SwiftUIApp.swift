27d73d4ea47010a62836435cc94ab0de70053517
Eduardo Andrade <kronnen0224@gmail.com>
2025-11-03 08:27:43 -0500

    Initial Commit

---
+++ SDP-Ejercicios-SwiftUI/SDP_Ejercicios_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDP_Ejercicios_SwiftUIApp.swift
+//  SDP-Ejercicios-SwiftUI
+//
+//  Created by Eduardo Andrade on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SDP_Ejercicios_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
